1de18b3e73608ee8fedc5259af68002724e96db0
gmdev86 <gmdev86@gmail.com>
2017-02-05 14:35:53 -0500

    update

--- TestProject/public/javascripts/indexMgr.js
+++ TestProject/public/javascripts/indexMgr.js
@@ -11,34 +11,10 @@
             type: "GET",
             async: true,
             success: function (result) {
-                //document.getElementById("divLoading").style.display = "none";
-                //document.getElementById("secTasks").style.display = "block";
-
-                console.log(result);
-                //var inject_folderTree = document.getElementById('inject_folderTree');
-                //inject_folderTree.innerHTML = result;
-
-                //make another ajax call passing the data from result to get the partial view
-                $.ajax({
-                    url: "http://localhost:1337/_folderTree",
-                    type: "POST",
-                    async: true,
-                    data: result,
-                    success: function (res) {
-                        document.getElementById("divLoading").style.display = "none";
-                        document.getElementById("secTasks").style.display = "block";
-
-                        console.log(res);
-                        var inject_folderTree = document.getElementById('inject_folderTree');
-                        inject_folderTree.innerHTML = res;
-
-                        //make another ajax call passing the data from result to get the partial view
-
-                    },
-                    error: function (e) {
-                        console.log(e);
-                    }
-                });
+                document.getElementById("divLoading").style.display = "none";
+                document.getElementById("secTasks").style.display = "block";
+                var inject_folderTree = document.getElementById('inject_folderTree');
+                inject_folderTree.innerHTML = result;
             },
             error: function (e) {
                 console.log(e);
