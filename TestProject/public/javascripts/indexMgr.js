f5b3e7d0bd4428fd96d1b80c7b7212ad005e3f93
Nathan M. Davis <davis8@rose-hulman.edu>
2017-02-07 16:58:09 -0500

    update

--- TestProject/public/javascripts/indexMgr.js
+++ TestProject/public/javascripts/indexMgr.js
@@ -1,20 +1,230 @@
 ﻿class IndexMgr {
     constructor() {
         var self = this;
-        self.getFolderTree();
+        //self.getGroups();
+        self.getProjectFolders();
+        //self.getFolderTree();
+        //self.getContacts();
+        //self.getUserById();
+    };
+
+    getGroups() {
+        var self = this;
+        //Load Folder Tree
+        $.ajax({
+            url: "http://localhost:1337/getGroups",
+            type: "GET",
+            async: true,
+            success: function (result) {
+                document.getElementById("mainLoading").style.display = "none";
+                document.getElementById("groupLoading").style.display = "none";
+                document.getElementById("secTasks").style.display = "block";
+                var inject_groups = document.getElementById('inject_groups');
+                inject_groups.innerHTML = result;
+                //self.getContacts();
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
     };
 
     getFolderTree() {
+        var self = this;
         //Load Folder Tree
         $.ajax({
             url: "http://localhost:1337/getFolderTree",
             type: "GET",
             async: true,
             success: function (result) {
-                document.getElementById("divLoading").style.display = "none";
+                document.getElementById("mainLoading").style.display = "none";
+                document.getElementById("folderTreeLoading").style.display = "none";
                 document.getElementById("secTasks").style.display = "block";
                 var inject_folderTree = document.getElementById('inject_folderTree');
                 inject_folderTree.innerHTML = result;
+                self.getContacts();
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    };
+
+    getProjectFolders() {
+        var self = this;
+        //Load Folder Tree
+        $.ajax({
+            url: "http://localhost:1337/getProjectFolders",
+            type: "GET",
+            async: true,
+            success: function (oData) {
+                //go get child data
+                //var voData = JSON.parse(oData);
+                $.ajax({
+                    url: "http://localhost:1337/loadChildFolderByIds",
+                    type: "POST",
+                    data: oData,
+                    contentType: 'application/json', //must have this: tells the server what type
+                    async: true,
+                    success: function (result) {                            
+                        document.getElementById("mainLoading").style.display = "none";
+                        document.getElementById("secTasks").style.display = "block";
+                        var inject_folderTree = document.getElementById('inject_folderTree');
+                        inject_folderTree.innerHTML = result;
+
+                        $(".projectFolders").on('click',function(event){
+                            var li = event.currentTarget;
+                            $(li).find("ul").toggleClass("in");
+                            console.log(li);
+                        });
+
+                        self.getChildFolders();
+                    },
+                    error: function (e) {
+                        console.log(e);
+                    }
+                });
+
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    };
+
+    getChildFolders(){
+        var self = this;
+        var lis = document.getElementsByClassName("projectFolders");
+        console.dir(lis);
+
+        for(var i = 0; i < lis.length; i++){
+            var folderId = lis[i].lastElementChild.innerText;
+            var me = lis[i];
+
+            var obj = {
+                "id": folderId
+            };
+
+            self.generateGrandChild(obj,me);            
+        };
+   
+    };
+
+    generateGrandChild(obj,me){
+        var self = this;
+        $.ajax({
+            url: "http://localhost:1337/getChildFolderById",
+            type: "POST",
+            data: JSON.stringify(obj),
+            contentType: 'application/json', //must have this: tells the server what type
+            async: true,
+            success: function (result) {
+
+                $.ajax({
+                    url: "http://localhost:1337/getGrandChildFolders",
+                    type: "POST",
+                    data: result,
+                    contentType: 'application/json', //must have this: tells the server what type
+                    async: true,
+                    success: function (oData) {
+
+                        if(oData != ""){
+                            var div = document.createElement("div");
+                            div.innerHTML = oData;
+                            me.appendChild(div);   
+                            $(".grandChildfolder").on('click',function(event){
+                                var li = event.currentTarget;
+                                var id = $(li).find("div").text();
+                                console.log(id);
+                                //Load tasks for folderId
+                                document.getElementById("tasksLoading").style.display = "block";
+                                self.loadTasksForFolder(id);
+                            });
+                        };
+
+                    },
+                    error: function (e) {
+                        console.log(e);
+                    }
+                });
+
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    };
+
+    loadTasksForFolder(folderId){
+        var self = this;
+
+        var data = {
+            "id": folderId
+        };
+
+        $.ajax({
+            url: "http://localhost:1337/loadTasksForFolder",
+            type: "POST",
+            async: true,
+            data: JSON.stringify(data),
+            contentType: 'application/json', //must have this: tells the server what type
+            success: function (result) {
+                document.getElementById("tasksLoading").style.display = "none";
+                var inject_tasks = document.getElementById('inject_tasks');
+                inject_tasks.innerHTML = result;
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        }); 
+
+    };
+
+    getContacts(){
+        var self = this;
+        $.ajax({
+            url: "http://localhost:1337/getContacts",
+            type: "GET",
+            async: true,
+            success: function (result) {
+                document.getElementById("contactsLoading").style.display = "none";
+                var inject_contacts = document.getElementById('inject_contacts');
+                inject_contacts.innerHTML = result;
+                self.getAccounts();
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    };
+
+    getAccounts(){
+        var self = this;
+        $.ajax({
+            url: "http://localhost:1337/getAccounts",
+            type: "GET",
+            async: true,
+            success: function (result) {
+                document.getElementById("accountsLoading").style.display = "none";
+                var inject_accounts = document.getElementById('inject_accounts');
+                inject_accounts.innerHTML = result;
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    };
+
+    getUserById(){
+        var self = this;
+        $.ajax({
+            url: "http://localhost:1337/getUserById",
+            type: "GET",
+            async: true,
+            success: function (result) {
+                document.getElementById("userLoading").style.display = "none";
+                var inject_user = document.getElementById('inject_user');
+                inject_user.innerHTML = result;
             },
             error: function (e) {
                 console.log(e);
--- TestProject/routes/index.js
+++ TestProject/routes/index.js
@@ -1,3 +1,8 @@
+/*
+    Known issues: if the user refreshes the page, the session is restarted and the access_token is lost.
+        fix: save the access_token in local db or indexed db.
+*/
+
 var express = require('express');
 var router = express.Router();
 var querystring = require('querystring');
@@ -50,8 +55,6 @@ router.get('/dashboard', function(req, res, next) {
             req.session['access_token'] = oData.access_token;
             req.session['token_type'] = oData.token_type;
             req.session['refresh_token'] = oData.refresh_token;
-
-            console.dir(req.session);
             oRes.render('dashboard', { pageTitle: 'testing the engine' });
         });
         pRes.on('error', function (err) {
@@ -69,12 +72,54 @@ router.get('/dashboard', function(req, res, next) {
     pReq.end();
 });
 
-router.get('/getFolderTree', function (req, res) {
+router.get('/getGroups', function (req, res) {
     var access_token = req.session['access_token'];
 
-    console.log('#########################');
-    console.log('access_token: ' + access_token);
-    console.log('#########################');
+    // console.log('#########################');
+    // console.log('access_token: ' + access_token);
+    // console.log('#########################');
+
+     var options = {
+       host: 'www.wrike.com',
+       port: 443,
+       path: '/api/v3/accounts/IEAAF6IL/groups',
+       method: 'GET',
+       headers: {
+           'Authorization': 'bearer ' + access_token
+       }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            var oData = JSON.parse(result);
+            var oDs = [];
+
+            for(var i = 0; i < oData["data"].length; i++){
+                var array = {};
+                array.id = oData["data"][i].id;
+                array.title = oData["data"][i].title;
+                oDs.push(array);
+            };   
+
+            var partial = res.render('_groups', { posts: oDs });
+            res.end(partial);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.get('/getFolderTree', function (req, res) {
+     var access_token = req.session['access_token'];
 
      var options = {
        host: 'www.wrike.com',
@@ -106,6 +151,395 @@ router.get('/getFolderTree', function (req, res) {
             };   
 
             var partial = res.render('_folderTree', { posts: oDs });
+            res.end(partial);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.get('/getProjectFolders', function (req, res) {
+     var access_token = req.session['access_token'];
+
+     var options = {
+       host: 'www.wrike.com',
+       port: 443,
+       path: '/api/v3/accounts/IEAAF6IL/folders',
+       method: 'GET',
+       headers: {
+           'Authorization': 'bearer ' + access_token
+       }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            var oData = JSON.parse(result);
+            var oDs = [];
+            var Root;
+
+            for(var i = 0; i < oData["data"].length; i++){
+                // Get the root looking at the title = Root
+                var title = oData["data"][i].title;                
+
+                if (title === "Root"){
+                    Root = oData["data"][i];
+                    break;
+                };
+
+            };   
+
+            res.end(JSON.stringify(Root));
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.post('/loadChildFolderByIds', function (req, res) {
+    var access_token = req.session['access_token'];
+    var body = req.body;
+    var ids = "";
+
+    for(var i = 0; i < body.childIds.length; i++){
+        ids += body.childIds[i];
+        if(i != body.childIds.length - 1){
+            ids += ",";
+        };        
+    };
+
+    // testing
+    console.log(ids);
+    var options = {
+        host: 'www.wrike.com',
+        port: 443,
+        path: '/api/v3/folders/' + ids,
+        method: 'GET',
+        headers: {
+            'Authorization': 'bearer ' + access_token
+        }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            var oData = JSON.parse(result);
+            var oDs = [];
+
+            for(var i = 0; i < oData["data"].length; i++){            
+                var total = oData["data"][i].childIds.length;
+                var array = {};
+                array.id = oData["data"][i].id;
+                array.title = oData["data"][i].title;
+                array.total = total;
+                oDs.push(array);
+            };  
+            
+            var partial = res.render('_folderTree', { posts: oDs });
+            res.end(partial);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.post('/getChildFolderById', function (req, res) {
+    var access_token = req.session['access_token'];
+    var body = req.body;
+
+    var options = {
+        host: 'www.wrike.com',
+        port: 443,
+        path: '/api/v3/folders/' + body.id,
+        method: 'GET',
+        headers: {
+            'Authorization': 'bearer ' + access_token
+        }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            res.end(result);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.post('/getGrandChildFolders', function (req, res) {
+    var access_token = req.session['access_token'];
+    var body = req.body;
+    var ids = "";
+
+    if(body.data[0].childIds.length > 0){
+
+        for(var i = 0; i < body.data[0].childIds.length; i++){
+            ids += body.data[0].childIds[i];
+            if(i != body.data[0].childIds.length - 1){
+                ids += ",";
+            };        
+        };
+
+        console.log(ids);
+        var options = {
+            host: 'www.wrike.com',
+            port: 443,
+            path: '/api/v3/folders/' + ids,
+            method: 'GET',
+            headers: {
+                'Authorization': 'bearer ' + access_token
+            }
+        };
+
+        https.request(options, function (response) {
+            var result = '';
+            response.on('data', function (chunk) {
+                console.log(chunk);
+                result += chunk;
+            });
+            response.on('end', function () {
+                console.log(result);
+                var oData = JSON.parse(result);
+                var oDs = [];
+
+                for(var i = 0; i < oData["data"].length; i++){            
+                    var total = oData["data"][i].childIds.length;
+                    var array = {};
+                    array.id = oData["data"][i].id;
+                    array.title = oData["data"][i].title;
+                    array.total = total;
+                    oDs.push(array);
+                };  
+                
+                var partial = res.render('_childFolders', { posts: oDs });
+                res.end(partial);
+            });
+            response.on('error', function (err) {
+                console.log(err);
+            });       
+        }).on('error', function (e) {
+        res.sendStatus(500);
+        }).end();      
+    } else {
+        res.end("");
+    };
+
+});
+
+router.post('/loadTasksForFolder', function (req, res) {
+     var access_token = req.session['access_token'];
+     var body = req.body;
+
+     var options = {
+       host: 'www.wrike.com',
+       port: 443,
+       path: '/api/v3/folders/' + body.id + '/tasks',
+       method: 'GET',
+       headers: {
+           'Authorization': 'bearer ' + access_token
+       }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            var oData = JSON.parse(result);
+            var oDs = [];
+
+            for(var i = 0; i < oData["data"].length; i++){
+                var array = {};
+                array.id = oData["data"][i].id;
+                array.title = oData["data"][i].title;
+                oDs.push(array);
+            };   
+
+            var partial = res.render('_tasks', { posts: oDs });
+            res.end(partial);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.get('/getContacts', function (req, res) {
+    var access_token = req.session['access_token'];
+
+    // console.log('#########################');
+    // console.log('access_token: ' + access_token);
+    // console.log('#########################');
+
+     var options = {
+       host: 'www.wrike.com',
+       port: 443,
+       path: '/api/v3/contacts?me=true',
+       method: 'GET',
+       headers: {
+           'Authorization': 'bearer ' + access_token
+       }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            var oData = JSON.parse(result);
+            var oDs = [];
+
+            for(var i = 0; i < oData["data"].length; i++){
+                var array = {};
+                var profiles = [];
+                var profile = {};
+                array.id = oData["data"][i].id;
+                array.firstName = oData["data"][i].firstName;
+                array.lastName = oData["data"][i].lastName;
+
+                for(var x = 0; x < oData["data"][i].profiles.length; x++){
+                    profile.accountId = oData["data"][i].profiles[x].accountId;
+                    profile.email = oData["data"][i].profiles[x].email;
+                    profile.role = oData["data"][i].profiles[x].role;
+                    profile.external = oData["data"][i].profiles[x].external;
+                    profile.admin = oData["data"][i].profiles[x].admin;
+                    profile.owner = oData["data"][i].profiles[x].owner;
+                    profiles.push(profile);
+                };
+
+                array.profiles = profiles;
+
+                oDs.push(array);
+            };   
+
+            var partial = res.render('_contacts', { posts: oDs });
+            res.end(partial);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+//getUserById -- not working
+router.get('/getUserById', function (req, res) {
+    var access_token = req.session['access_token'];
+    // console.log('#########################');
+    // console.log('access_token: ' + access_token);
+    // console.log('#########################');
+
+     var options = {
+       host: 'www.wrike.com',
+       port: 443,
+       path: '/api/v3/users/KUACVP23',
+       method: 'GET',
+       headers: {
+           'Authorization': 'bearer ' + access_token
+       }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            var oData = JSON.parse(result);
+            var oDs = [];
+
+            for(var i = 0; i < oData["data"].length; i++){
+                var array = {};
+                array.id = oData["data"][i].id;
+                array.firstName = oData["data"][i].firstName;
+                array.lastName = oData["data"][i].lastName;
+                oDs.push(array);
+            };   
+
+            var partial = res.render('_user', { posts: oDs });
+            res.end(partial);
+        });
+        response.on('error', function (err) {
+            console.log(err);
+        });       
+    }).on('error', function (e) {
+       res.sendStatus(500);
+    }).end();
+});
+
+router.get('/getAccounts', function (req, res) {
+    var access_token = req.session['access_token'];
+    // console.log('#########################');
+    // console.log('access_token: ' + access_token);
+    // console.log('#########################');
+
+     var options = {
+       host: 'www.wrike.com',
+       port: 443,
+       path: '/api/v3/accounts',
+       method: 'GET',
+       headers: {
+           'Authorization': 'bearer ' + access_token
+       }
+    };
+
+    https.request(options, function (response) {
+        var result = '';
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            result += chunk;
+        });
+        response.on('end', function () {
+            console.log(result);
+            var oData = JSON.parse(result);
+            var oDs = [];
+
+            for(var i = 0; i < oData["data"].length; i++){
+                var array = {};
+                array.id = oData["data"][i].id;
+                array.name = oData["data"][i].name;
+                oDs.push(array);
+            };   
+
+            var partial = res.render('_accounts', { posts: oDs });
             res.end(partial);
         });
         response.on('error', function (err) {
